35dc4c80a18eaefe5e103acf60e21f1f9a49796c
RichaAgarwal417 <46071670+RichaAgarwal417@users.noreply.github.com>
2019-01-08 11:59:35 -0500

    Commit2

--- AmazonSelenium/src/com/selenium/amazon/AmazonTest.java
+++ AmazonSelenium/src/com/selenium/amazon/AmazonTest.java
@@ -1,8 +1,16 @@
 package com.selenium.amazon;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import com.selenium.amazonConfigs.*;
 
 public class AmazonTest {
 	WebDriver dr;
@@ -11,17 +19,26 @@ public class AmazonTest {
 		dr = new FirefoxDriver();
 		wait = new WebDriverWait(dr,10);
 	}
+	
 	public static void main(String[] args) throws InterruptedException {
+
 		AmazonTest at = new AmazonTest();
+		ReadTestData rtd = new ReadTestData();
 		Login lg =new Login();
 		ProductBrowse pb = new ProductBrowse();
-		lg.signIn(at.dr, "https://www.amazon.ca", "<Enter your amazon username>", "<enter your pswd>");
+		
+		String URL = rtd.getURL();
+		String Username = rtd.getUsername();
+		String Password = rtd.getPassword();
+		
+		lg.signIn(at.dr, URL, Username, Password);
 		pb.productSearch(at.dr, at.wait, "Sneakers ");
 		pb.sort(at.dr, "Average");
+		Thread.sleep(2000);
 		//pb.selectShoeClosureType(at.dr, at.wait, "lace");
 		pb.clickOnProduct(at.dr, at.wait, "Clarks Women's Wave Trek Lace-Up Fashion Sneaker");
-		pb.SelectSizeOfTheProduct(at.dr, "10 M US");
-		pb.AddToCart(at.dr, at.wait);
+		//pb.SelectSizeOfTheProduct(at.dr, "10 M US");
+		pb.AddToCart(at.dr, at.wait, "10 M US");
 		pb.selectUserOption(at.dr, "cart", at.wait);
 	}
 }
